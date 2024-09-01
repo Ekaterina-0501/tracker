176e17dbc32e5572ebd0be3a52c586bb7aa72917
Ekaterina-0501 <Ekaterina-0501@example.com>
2024-09-01 17:25:48 +0300

    9.3 Массивы и модели

--- src/main/java/ru/pojo/Library.java
+++ src/main/java/ru/pojo/Library.java
@@ -6,28 +6,21 @@ public class Library {
         Book textbook = new Book("Clean code", 884);
         Book fantasy = new Book("Armageddon", 556);
         Book instruction = new Book("Vacuum cleaner", 28);
-        Book[] books = new Book[4];
-        books[0] = novel;
-        books[1] = textbook;
-        books[2] = fantasy;
-        books[3] = instruction;
-        for (int i = 0; i < books.length; i++) {
-            Book book = books[i];
+        Book[] books = {novel, textbook, fantasy, instruction};
+        for (Book book : books) {
             System.out.println(book.getName() + "  " + book.getPages() + " pages");
         }
-        System.out.println("");
+        System.out.println();
         System.out.println("Swapped 0 and 3");
         Book change = books[0];
         books[0] = books[3];
         books[3] = change;
-        for (int i = 0; i < books.length; i++) {
-            Book book = books[i];
+        for (Book book : books) {
             System.out.println(book.getName() + "  " + book.getPages() + " pages");
         }
-        System.out.println("");
+        System.out.println();
         System.out.println("Output Clean code");
-        for (int i = 0; i < books.length; i++) {
-            Book book = books[i];
+        for (Book book : books) {
             if (book.getName().equals("Clean code")) {
                 System.out.println(book.getName() + "  " + book.getPages() + " pages");
             }
