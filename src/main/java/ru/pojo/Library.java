4341f3744ff49537689b14b41be9fdaf471b662d
Ekaterina-0501 <Ekaterina-0501@example.com>
2024-09-16 17:31:13 +0300

    9.3 Массивы и модели

--- src/main/java/ru/pojo/Library.java
+++ src/main/java/ru/pojo/Library.java
@@ -21,7 +21,7 @@ public class Library {
         System.out.println();
         System.out.println("Output Clean code");
         for (Book book : books) {
-            if (book.getName().equals("Clean code")) {
+            if ("Clean code".equals(book.getName())) {
                 System.out.println(book.getName() + "  " + book.getPages() + " pages");
             }
         }
